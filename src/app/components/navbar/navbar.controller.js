f6622e68db94d3389b966ab8478c27be123255fb
boozz <boozzd@gmail.com>
2015-05-18 23:21:04 +0300

    Init app commit

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('puzzle')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('puzzle', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+    $urlRouterProvider.otherwise('/');
+  })
+;
